f1f6f83df3670a8c92792c79d441539ed95ae4e0
RicardoRblog <67088887+RicardoRblog@users.noreply.github.com>
2023-02-06 18:20:35 -0500

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,28 @@
+jQuery(document).ready(listo);
+
+function listo()
+{
+    jQuery(".hamb").click(function(ABRIR){
+        ABRIR.preventDefault();
+        jQuery(".menu").toggleClass("open");
+
+        jQuery(".hamb i").toggleClass("fa-times");
+
+    });
+
+    jQuery("header .menu a").click(function(){
+
+        jQuery(".menu").removeClass("open");
+        jQuery(".hamb i").removeClass("fa-times");
+
+        var seccion = jQuery(this).attr("href");
+        
+        jQuery("html,body").animate({
+            "scrollTop": jQuery(dev).offset().top
+
+        })
+
+    })
+
+
+}
